9dcd6a1e7f819b0f43bbb50d8567a10f48382dbf
Daksh <dakshmaroo11@gmail.com>
2024-10-19 10:50:29 +0530

    Done and Dusted

--- app.js
+++ app.js
@@ -9,14 +9,18 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/api/servics", serviceRoutes);
+app.use("/", serviceRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API is running....");
+});
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ success: false, message: "Something Went Wrong" });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
--- config/db.js
+++ config/db.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        const conn = await mongoose.connect(process.env.MONGO_URI).then(() => {
+            console.log("Database connected");
+        }).catch((err) => {
+            console.log(err);
+        });
+        // console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
         console.log(`Error : ${err.message}`);
         process.exit(1);
